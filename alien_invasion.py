0bf39808b8bff0f98fe62ca1d507a754f86c9386
Shanoxx <lukasgrot98@gmail.com>
2024-02-22 11:35:21 +0100

    Project start - Alien Invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     "Ogolna klasa przeznaczona do zarzadzania zasobami i sposobem dzialania gry."
 
@@ -9,8 +11,10 @@ class AlienInvasion:
         "Inicjalizacja gry i utworzenie jej zasobow"
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja Obcych")
 
         #Zdefiniowanie koloru tla.
@@ -24,7 +28,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #Odswiezanie ekranu w trakcie kazdej iteracji petli.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             #Wyswietlenie ostatnio zmodyfikowanego ekranu.
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Klasa przeznaczona do przechowywania wszystkich ustawien gry."""
+
+    def __init__(self):
+        """Inicjalizacja ustawien gry."""
+        #Ustawienia ekranu.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230 ,230)
