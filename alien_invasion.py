da7da92c763445827a237ef6ef0f9dd3268eda92
Shanoxx <lukasgrot98@gmail.com>
2024-02-25 20:41:31 +0100

    Ship movement setting

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,5 @@
 import sys 
-
 import pygame
-
 from settings import Settings
 from ship import Ship
 
@@ -18,14 +16,12 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja Obcych")
         self.ship = Ship(self)
-        
-        #Zdefiniowanie koloru tla.
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         "Rozpoczecie petli glownej gry"
         while True:     
             self._chceck_events()
+            self.ship.update()
             self._update_screen()     
             self.clock.tick(60)
 
@@ -34,6 +30,26 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Reakcja na nacisniecie klawisza."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key ==pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Reakcja na zwolnienie klawisza"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     
     def _update_screen(self):
         #Odswiezanie ekranu w trakcie kazdej iteracji petli.
--- ship.py
+++ ship.py
@@ -1,11 +1,12 @@
 import pygame
 
 class Ship:
-    """Klasa przeznaczona do zarzadzania sttatkiem kosmicznym."""
+    """Klasa przeznaczona do zarzadzania statkiem kosmicznym."""
 
     def __init__(self, ai_game):
         """Inicjalizacja statku kosmicznego i jego polozenie poczatkowe."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Wczytywanie obrazu statku kosmicznego i pobranie jego prostokata.
@@ -14,6 +15,30 @@ class Ship:
 
         #Kazdy nowy statek kosmiczny pojawia sie na dole ekranu.
         self.rect.midbottom = self.screen_rect.midbottom
+        #Polozenie poziome statku jest przechowywane w postaci liczby zmiennoprzecinkowej.
+        self.x = float(self.rect.x)
+
+        #Opcje wskazujace na poruszanie sie statku.
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        """
+        Uaktualnienie polozenia statku na podstawie opcji wskazujacej na jego ruch.
+        """
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        #Uaktualnienie wartosci wspolrzednej X statku, a nie jego prostokata.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+           self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed 
+
+        #Uaktualnienie obiektu rect na podstawie wartosci self.x.
+        self.rect.x = self.x
+
 
     def blitme(self):
         """Wyswietlenie statku kosmicznego w jego aktualnym polozeniu."""
